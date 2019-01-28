6a7ee21291a804996a996b3c11300604125faa03
nikoladim123 <nikoladimitrijevic1991@gmail.com>
2019-01-28 18:17:31 +0100

    car two lib zooming

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var img = document.getElementsByClassName('img')[0];
 var smallImg = document.getElementsByClassName('smallImg');
 
+img.onmousemove = function(e) {
+    var xCoor = e.clientX;
+    var yCoor = e.clientY;
+    img.style.backgroundPosition = -xCoor + 'px ' + -yCoor + 'px';
+}
+img.onmouseleave = function(){
+  img.style.backgroundPosition = '0 0';
+}
+
+
 for (var i = 0; i < smallImg.length ; i++) {
   smallImg[i].addEventListener('click',function(e){
     img.style.opacity = '0';
